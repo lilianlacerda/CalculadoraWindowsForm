6718fe995dc7de4c12f0dde14345a94d63ee4e21
lilianlacerda <lilianraiany@gmail.com>
2025-03-02 20:22:11 -0300

    Criar métodos para converter os valores dos textbox para float e para validar se os campos estão vazios

--- Form1.cs
+++ Form1.cs
@@ -7,9 +7,9 @@ namespace CalculadoraWindowsForm
             InitializeComponent();
         }
 
-        float numero1;
-        float numero2;
-        float resultado;
+        public float numero1;
+        public float numero2;
+        public float resultado;
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -23,22 +23,30 @@ namespace CalculadoraWindowsForm
 
         private void btnSoma_Click(object sender, EventArgs e)
         {
-
+            ValidarCampos();
+            resultado = Somar(numero1, numero2);
+            label1.Text = $"{numero1} + {numero2} = {resultado}";
         }
 
         private void btnSubtracao_Click(object sender, EventArgs e)
         {
-
+            ValidarCampos();
+            resultado = Subtrair(numero1, numero2);
+            label1.Text = $"{numero1} - {numero2} = {resultado}";
         }
 
         private void btnMultiplicacao_Click(object sender, EventArgs e)
         {
-
+            ValidarCampos();
+            resultado = Multiplicar(numero1, numero2);
+            label1.Text = $"{numero1} * {numero2} = {resultado}";
         }
 
         private void btnDivisao_Click(object sender, EventArgs e)
         {
-
+            ValidarCampos();
+            resultado = Dividir(numero1, numero2);
+            label1.Text = $"{numero1} / {numero2} = {resultado}";
         }
 
         public static float Somar(float numero1, float numero2)
@@ -63,6 +71,24 @@ namespace CalculadoraWindowsForm
         {
             float resultado = numero1 / numero2;
             return resultado;
+        }
+
+        private void ConverterValoresFloat()
+        {
+            numero1 = float.Parse(textBox1.Text);
+            numero2 = float.Parse(textBox2.Text);
+        }
+
+        private void ValidarCampos()
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                label1.Text = ("Preencha os campos corretamente!");
+            }
+            else
+            {
+                ConverterValoresFloat();
+            }
         }
     }
 }
